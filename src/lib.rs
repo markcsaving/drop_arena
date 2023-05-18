d07f001ecb1fe420e881a96860af655cb1753d16
Mark Saving <marksaving@gmail.com>
2023-05-18 17:03:55 -0500

    Modified features

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 #![warn(missing_docs)]
-#![cfg_attr(not(test), no_std)]
+#![cfg_attr(not(any(feature = "std", test)), no_std)]
 
+// After modifying the below, please make sure to run
+// $ cargo readme > README.md
 //! # Introduction
 //! A [`DropArena<T>`] can allocate or deallocate individual elements of type `T`. Only allocating elements of a fixed size
 //!! and alignment allows the allocator to be extremely efficient compared to an ordinary implementation of `malloc` and `free`.
 //! Think of [`DropArena`] as providing a combination of the functionality of an [`Arena`] and the allocator that makes Boxes.
 //! 
-//! The [`DropArena`] can return a [`DropBox<T>`], which functions very much like a `Box<T>` except for being tied to the 
+//! The [`DropArena`] can return a [`DropBox<T>`], which functions very much like a [`Box<T>`] except for being tied to the
 //! [`DropArena`] that allocated it. A [`DropBox`] can be used exactly like a [`&mut T`]; in fact, it is a `repr(transparent)`
 //! wrapper around a [`&mut T`]. 
 //! 
@@ -17,7 +19,7 @@
 //! Finally, you may call [`DropBox::leak`], which produces a [`&mut T`]. This means that unless unsafe code is used, the `T` will
 //! never be [`drop`]ed. However, when the [`DropArena`] which allocated the [`DropBox`] is dropped, the memory will be reclaimed.
 //! 
-//! In order to reclaim the memory allocated to a [`DropBox<T>`, we need a reference to the [`DropArena<T>`] which allocated it.
+//! In order to reclaim the memory allocated to a [`DropBox<T>`], we need a reference to the [`DropArena<T>`] which allocated it.
 //! We can use the [`DropArena::drop_box`] method on a [`DropBox`] to drop the underlying value and reclaim the memory. We can 
 //! also use the [`DropArena::box_into_inner`] method to retrieve the underlying `T` from a [`DropBox<T>`] and reclaim the memory
 //! it used.
@@ -118,7 +120,7 @@
 //! but exhaustive fuzzing is needed. This code uses a fair amount of `unsafe`, and that means there are plenty of chances for 
 //! serious bugs to appear. I think I've caught most of them, but it's not impossible I neglected one.
 //!
-//! Making sure that a [`DropBox<T>`] implements all the traits that a `Box<T>` does seems desirable.
+//! Making sure that a [`DropBox<T>`] implements all the traits that a [`Box<T>`] does seems desirable.
 //!
 //! # Maintenance
 //!
